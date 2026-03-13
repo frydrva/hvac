60f1b78075ea2e14268fb49970edbf36d95f42cf
Václav Frydrych <v34frydrych@gmail.com>
2026-03-13 10:13:47 +0100

    peni

--- carProduct.php
+++ carProduct.php
@@ -49,7 +49,9 @@
 
   $allRecords = Db::queryAll('SELECT * FROM pauta');
   
-  
+  if (isset($_GET['id'])) {
+    $car = Db::queryOne('SELECT * FROM pauta WHERE id = ?', [$_GET['id']])
+  }
 
   ?>
 </head>
@@ -171,128 +173,16 @@
 <!-- Breadcrumb Begin -->
 
 <!-- Car Section Begin -->
-<section class="car spad">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3">
-        <div class="car__sidebar">
-          <div class="car__search">
-            <h5>Car Search</h5>
-            <form action="#">
-              <input type="text" placeholder="Search...">
-              <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
-          </div>
-          <div class="car__filter">
-            <h5>Car Filter</h5>
-            <form action="#">
-              <select>
-                <option data-display="Brand">Select Brand</option>
-                <option value="">Acura</option>
-                <option value="">Audi</option>
-                <option value="">Bentley</option>
-                <
-                <option value="">BMW</option>
-                <option value="">Bugatti</option>
-              </select>
-              <select>
-                <option data-display="Model">Select Model</option>
-                <option value="">Q3</option>
-                <option value="">A4</option>
-                <option value="">AVENTADOR</option>
-              </select>
-              <select>
-                <option value="">Body Style</option>
-                <option value="">Option 1</option>
-                <option value="">Option 2</option>
-              </select>
-              <select>
-                <option value="">Condition</option>
-                <option value="">First Hand</option>
-                <option value="">Second Hand</option>
-              </select>
-              <select>
-                <option value="">Transmisson</option>
-                <option value="">Bluetooth</option>
-                <option value="">WiFi</option>
-              </select>
-              <select>
-                <option value="">Mileage</option>
-                <option value="">27</option>
-                <option value="">20</option>
-                <option value="">15</option>
-                <option value="">10</option>
-              </select>
-              <select>
-                <option value="">Engine</option>
-                <option value="">BS3</option>
-                <option value="">BS4</option>
-                <option value="">BS5</option>
-                <option value="">BS6</option>
-              </select>
-              <select>
-                <option value="">Colors</option>
-                <option value="">Red</option>
-                <option value="">Blue</option>
-                <option value="">Black</option>
-                <option value="">Yellow</option>
-              </select>
-              <div class="filter-price">
-                <p>Price:</p>
-                <div class="price-range-wrap">
-                  <div class="filter-price-range"></div>
-                  <div class="range-slider">
-                    <div class="price-input">
-                      <input type="text" id="filterAmount">
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="car__filter__btn">
-                <button type="submit" class="site-btn">Reset FIlter</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-9">
-        <div class="car__filter__option">
-          <div class="row">
-            <div class="col-lg-6 col-md-6">
-              <div class="car__filter__option__item">
-                <h6>Show On Page</h6>
-                <select>
-                  <option value="">9 Car</option>
-                  <option value="">15 Car</option>
-                  <option value="">20 Car</option>
-                </select>
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6">
-              <div class="car__filter__option__item car__filter__option__item--right">
-                <h6>Sort By</h6>
-                <select>
-                  <option value="">Price: Highest Fist</option>
-                  <option value="">Price: Lowest Fist</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
+
           
           <?php
           if ($allRecords) {
-            foreach ($allRecords as $record) {
-              echo('  
+            echo('  
             
                 <div class="col-lg-4 col-md-4">
                   <div class="car__item">
                     <div class="car__item__pic__slider owl-carousel">
                       <img src="img/cars/car-3.jpg" alt="" loading="lazy" decoding="async">
-                      <img src="img/cars/car-8.jpg" alt="" loading="lazy" decoding="async">
-                      <img src="img/cars/car-6.jpg" alt="" loading="lazy" decoding="async">
-                      <img src="img/cars/car-5.jpg" alt="" loading="lazy" decoding="async">
                     </div>
                     <div class="car__item__text">
                       <div class="car__item__text__inner">
@@ -311,8 +201,7 @@
                     </div>
                   </div>
                 </div>
-            ');
-            }
+            ');            
           }
             ?>
         </div>
